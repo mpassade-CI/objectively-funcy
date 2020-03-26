1923233e247479aae21b01d4cce2a3b101fa0bc2
Michael Passade <michael.passade@codeimmersives.com>
2020-03-26 12:19:38 -0400

    done

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 function getFirstName(person) {
+  return person.firstName
 }
 
 function getLastName(person) {
+  return person.lastName
 }
 
 function getFullName(person) {
+  return person.firstName+' '+person.lastName
 }
 
 function setFirstName(person, name) {
+  person.firstName = name
 }
 
 function setAge(person, age) {
+  person.age = age
 }
 
 function giveBirthday(person) {
+  person.age===undefined ? person.age=1 : person.age++
 }
 
 function marry(person1, person2) {
+  person1.married = true
+  person2.married = true
+  person1.spouseName = getFullName(person2)
+  person2.spouseName = getFullName(person1)
 }
 
 function divorce(person1, person2) {
+  person1.married = false
+  person2.married = false
+  delete person1.spouseName
+  delete person2.spouseName
 }
 
 module.exports = {
